3c9706dce3c42f277a068367e4d389fdd7b32ed5
Matthew Brunelle <ciferkey@gmail.com>
2025-11-25 20:02:10 -0500

    test update.

--- 2_echor/echor/tests/cli.rs
+++ 2_echor/echor/tests/cli.rs
@@ -3,6 +3,19 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use anyhow::Result;
 
+fn run(args: &[&str], expected_file: &str) -> Result<()> {
+    let expected = fs::read_to_string(expected_file)?;
+    let output = Command::cargo_bin("echor")?
+        .args(args)
+        .output()
+        .expect("fail");
+
+    let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
+    assert_eq!(stdout, expected);
+
+    Ok(())
+}
+
 #[test]
 fn dies_no_args() -> Result<()> {
     let mut cmd = Command::cargo_bin("echor")?;
@@ -16,22 +29,21 @@ fn dies_no_args() -> Result<()> {
 
 #[test]
 fn hello1() -> Result<()> {
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile)?;
-
-    let mut cmd = Command::cargo_bin("echor")?;
-
-    cmd.arg("Hello there").assert().success().stdout(expected);
-    Ok(())
+    run(&["Hello there"], "tests/expected/hello1.txt")
 }
 
 #[test]
 fn hello2() -> Result<()> {
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile)?;
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
 
-    let mut cmd = Command::cargo_bin("echor")?;
+#[test]
+fn hello1_no_newline() -> Result<()> {
+    run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
 
-    cmd.args(["Hello", "there"]).assert().success().stdout(expected);
-    Ok(())
+
+#[test]
+fn hello2_no_newline() -> Result<()> {
+    run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
 }
