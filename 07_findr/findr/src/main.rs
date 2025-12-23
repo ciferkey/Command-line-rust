36a675cc4434d9f40585971e3fd15af5ec82d056
Matthew Brunelle <ciferkey@gmail.com>
2025-12-23 11:04:15 -0500

    added chapter 7

---
+++ 07_findr/findr/src/main.rs
@@ -0,0 +1,120 @@
+use std::any::Any;
+use regex::Regex;
+use clap::{arg, Parser, builder::PossibleValue, ValueEnum, ArgAction};
+use anyhow::Result;
+use std::fs::{File};
+use std::io::{self, BufRead, BufReader, Write};
+use walkdir::{DirEntry, WalkDir};
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+enum EntryType {
+    Dir,
+    File,
+    Link,
+}
+
+impl ValueEnum for EntryType {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[EntryType::Dir, EntryType::File, EntryType::Link]
+    }
+
+    fn to_possible_value(&self) -> Option<PossibleValue> {
+        Some(match self {
+            EntryType::Dir => PossibleValue::new("d"),
+            EntryType::File => PossibleValue::new("f"),
+            EntryType::Link => PossibleValue::new("l"),
+        })
+    }
+}
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+struct Args {
+
+    #[arg(value_name = "PATH", default_value = ".")]
+    paths: Vec<String>,
+
+    #[arg(
+        short,
+        long("name"),
+        value_name = "NAME",
+        value_parser(Regex::new),
+        action(ArgAction::Append),
+        num_args(0..),
+    )]
+    names: Vec<Regex>,
+
+    #[arg(
+        short('t'),
+        long("type"),
+        value_name = "TYPE",
+        value_parser(clap::value_parser!(EntryType)),
+        action(ArgAction::Append),
+        num_args(0..),
+    )]
+    entry_types: Vec<EntryType>,
+}
+
+fn print(args: &Args, entry: DirEntry) -> Result<()>  {
+
+    if !args.names.is_empty() {
+        let path = entry.file_name().to_string_lossy();
+
+        let is_match = args.names.iter().map(|regex| regex.is_match(&path)).any(|v| v);
+        if !is_match {
+            return Ok(())
+        }
+    }
+
+    if !args.entry_types.is_empty() {
+        let file_type = entry.file_type();
+
+        let mut matches = false;
+        for entry_type in &args.entry_types {
+            match entry_type {
+                EntryType::Dir => {
+                    if file_type.is_dir() {
+                        matches = true;
+                    }
+                }
+                EntryType::File => {
+                    if file_type.is_file() {
+                        matches = true;
+                    }
+                }
+                EntryType::Link => {
+                    if file_type.is_symlink() {
+                        matches = true;
+                    }
+                }
+            }
+        }
+        if !matches {
+            return Ok(())
+        }
+    }
+
+    println!("{}", entry.path().display());
+
+    Ok(())
+}
+
+fn run(args: Args) -> Result<()> {
+    for path in &args.paths {
+        for entry in WalkDir::new(path) {
+            match entry {
+                Err(e) => eprintln!("{e}"),
+                Ok(entry) => print(&args, entry)?,
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+}
