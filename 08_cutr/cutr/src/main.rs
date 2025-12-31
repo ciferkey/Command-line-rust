af6ac2749c550a9e19c259afa15105094beb2272
Matthew Brunelle <ciferkey@gmail.com>
2025-12-31 11:43:07 -0500

    All tests passing.

--- 08_cutr/cutr/src/main.rs
+++ 08_cutr/cutr/src/main.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 use std::fs::{File};
 use std::io::{self, BufRead, BufReader};
 use std::ops::Range;
-use regex::Regex;
+use csv::{ReaderBuilder, StringRecord, WriterBuilder};
 
 type PositionList = Vec<Range<usize>>;
 
@@ -44,6 +44,38 @@ struct Args {
 
     #[command(flatten)]
     extract: ArgsExtract,
+}
+
+fn extract_chars(line: &str, char_pos: &[Range<usize>]) -> String {
+    char_pos.iter()
+        .map(|range| line.chars()
+            .skip(range.start)
+            .take(range.len())
+            .collect::<String>())
+        .collect()
+}
+
+fn extract_bytes(line: &str, byte_pos: &[Range<usize>]) -> String {
+    byte_pos.iter()
+        .map(|range| {
+            let bytes: Vec<u8> = line.bytes()
+                .skip(range.start)
+                .take(range.len())
+                .collect();
+
+            String::from_utf8_lossy(&bytes).into_owned()
+        })
+        .collect()
+}
+
+fn extract_fields(record: &StringRecord, field_pos: &[Range<usize>]
+) -> Vec<String> {
+    let fields: Vec<_> = record.iter().collect();
+    field_pos.iter()
+        .cloned()
+        .flat_map(|range| range.filter_map(|i| fields.get(i)))
+        .map(|s| (*s).to_string())
+        .collect()
 }
 
 fn check_list_value(whole: &str, part: &str) -> Result<usize> {
@@ -111,13 +143,68 @@ fn open(filename: &str) -> Result<Box<dyn BufRead>> {
 
 fn run(args: Args) -> Result<()> {
 
-    if args.delimiter.as_bytes().len() > 1 {
-        bail!(r#"--delib "{}" must be a single byte"#, args.delimiter);
+    if args.delimiter.as_bytes().len() != 1 {
+        bail!(r#"--delim "{}" must be a single byte"#, args.delimiter);
     }
 
     let delimiter: u8 = *args.delimiter.as_bytes().first().unwrap();
 
-    println!("{:#?}", args);
+    let extract = if let Some(fields) =
+        args.extract.fields.map(parse_pos).transpose()?
+    {
+        Extract::Fields(fields)
+    } else if let Some(bytes) =
+        args.extract.bytes.map(parse_pos).transpose()?
+    {
+        Extract::Bytes(bytes)
+    } else if let Some(chars) =
+        args.extract.chars.map(parse_pos).transpose()?
+    {
+        Extract::Chars(chars)
+    } else {
+        unreachable!("Must have --fields, --bytes, or --chars");
+    };
+
+    for filename in &args.files {
+        match open(filename) {
+            Err(err) => eprintln!("{filename}: {err}"),
+            Ok(file) => match &extract {
+                Extract::Fields(field_pos) => {
+                    let mut reader = ReaderBuilder::new()
+                        .delimiter(delimiter)
+                        .has_headers(false)
+                        .from_reader(file);
+
+                    for record in reader.records() {
+                        let fields = extract_fields(&record?, field_pos);
+
+                        let mut writer = WriterBuilder::new()
+                            .delimiter(delimiter)
+                            .from_writer(vec![]);
+
+                        writer.write_record(fields);
+
+                        let line = String::from_utf8(writer.into_inner()?)?;
+
+                        print!("{}", line);
+                    }
+                }
+                Extract::Bytes(byte_pos) => {
+                    for line in file.lines() {
+                        let line = &line?;
+                        println!("{}", extract_bytes(line, byte_pos));
+                    }
+                }
+                Extract::Chars(char_pos) => {
+                    for line in file.lines() {
+                        let line = &line?;
+                        println!("{}", extract_chars(line, char_pos));
+                    }
+                }
+            },
+        }
+    }
+
     Ok(())
 }
 
@@ -130,7 +217,44 @@ fn main() {
 
 #[cfg(test)]
 mod unit_tests {
-    use super::parse_pos;
+    use super::{parse_pos, extract_chars, extract_bytes, extract_fields};
+    use csv::StringRecord;
+
+    #[test]
+    fn test_extract_fields() {
+        let rec = StringRecord::from(vec!["Captain", "Sham", "12345"]);
+        assert_eq!(extract_fields(&rec, &[0..1]), &["Captain"]);
+        assert_eq!(extract_fields(&rec, &[1..2]), &["Sham"]);
+        assert_eq!(
+            extract_fields(&rec, &[0..1, 2..3]),
+            &["Captain", "12345"]
+        );
+        assert_eq!(extract_fields(&rec, &[0..1, 3..4]), &["Captain"]);
+        assert_eq!(extract_fields(&rec, &[1..2, 0..1]), &["Sham", "Captain"]);
+    }
+
+    #[test]
+    fn test_extract_chars() {
+        assert_eq!(extract_chars("", &[0..1]), "".to_string());
+        assert_eq!(extract_chars("ábc", &[0..1]), "á".to_string());
+        assert_eq!(extract_chars("ábc", &[0..1, 2..3]), "ác".to_string());
+        assert_eq!(extract_chars("ábc", &[0..3]), "ábc".to_string());
+        assert_eq!(extract_chars("ábc", &[2..3, 1..2]), "cb".to_string());
+        assert_eq!(
+            extract_chars("ábc", &[0..1, 1..2, 4..5]),
+            "áb".to_string()
+        );
+    }
+
+    #[test]
+    fn test_extract_bytes() {
+        assert_eq!(extract_bytes("ábc", &[0..1]), "�".to_string());
+        assert_eq!(extract_bytes("ábc", &[0..2]), "á".to_string());
+        assert_eq!(extract_bytes("ábc", &[0..3]), "áb".to_string());
+        assert_eq!(extract_bytes("ábc", &[0..4]), "ábc".to_string());
+        assert_eq!(extract_bytes("ábc", &[3..4, 2..3]), "cb".to_string());
+        assert_eq!(extract_bytes("ábc", &[0..2, 5..6]), "á".to_string());
+    }
 
     #[test]
     fn test_parse_pos() {
